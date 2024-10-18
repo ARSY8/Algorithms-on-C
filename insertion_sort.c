cf45d783720586238674f3f85d54b3cbc1a57861
A. Yudintsev <arsenijudincev30350@gmail.com>
2024-10-18 23:33:17 +0700

    Update insertion_sort.c

--- insertion_sort.c
+++ insertion_sort.c
@@ -7,7 +7,7 @@ typedef enum {
 	type_char,
 } data_type;
 
-void insertion_sort(void* arr, int len, size_t size_in_bytes, data_type type);
+void insertion_sort(void* arr, int len, data_type type);
 void swap(void* a, void* b, size_t size);
 int compare_value(void* a, void* b, data_type type);
 
@@ -17,19 +17,45 @@ int main(void) {
 	float float_arr[] = { 3.14, -1.5, 0.0, 2.71, -8.25, 5.5 };
 	char char_arr[] = { "Hello World" };
 
-	size_t size_data_type = sizeof(char_arr[0]);
+	char data_type = 'c';
 
-	int len = sizeof(char_arr) / size_data_type;
-	insertion_sort(char_arr, len, size_data_type, type_char);
-	
-	for (int i = 0; i < len; ++i) {
-		printf("%c", char_arr[i]);
+	if (data_type == 'i') {
+		int len = sizeof(int_arr) / sizeof(int_arr[0]);
+		insertion_sort(int_arr, len, type_char);
+		for (int i = 0; i < len; ++i) {
+			printf("%d", int_arr[i]);
+		}
+	}
+	else if (data_type == 'f') {
+		int len = sizeof(float_arr) / sizeof(float_arr[0]);
+		insertion_sort(float_arr, len, type_float);
+		for (int i = 0; i < len; ++i) {
+			printf("%f", float_arr[i]);
+		}
+	}
+	else if (data_type == 'c') {
+		int len = sizeof(char_arr) / sizeof(char_arr[0]);
+		insertion_sort(char_arr, len, type_char);
+		for (int i = 0; i < len; ++i) {
+			printf("%c", char_arr[i]);
+		}
 	}
 	return 0;
 }
 
-void insertion_sort(void* arr, int len, size_t size_in_bytes, data_type type){
-
+void insertion_sort(void* arr, int len, data_type type){
+	size_t size_in_bytes;
+	switch (type) {
+	case type_int:
+		size_in_bytes = sizeof(int);
+		break;
+	case type_float:
+		size_in_bytes = sizeof(float);
+		break;
+	case type_char:
+		size_in_bytes = sizeof(char);
+		break;
+	}
 	for (int i = 1; i < len; i++) {
 		for (int j = i; j > 0; --j) {
 			void* current = (char*)arr + j * size_in_bytes;
