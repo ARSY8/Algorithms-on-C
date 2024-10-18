3e6db20567c8c4ff7b595ff17937572967ac9cb1
A. Yudintsev <arsenijudincev30350@gmail.com>
2024-10-18 23:59:34 +0700

    Update insertion_sort.c

--- insertion_sort.c
+++ insertion_sort.c
@@ -10,52 +10,43 @@ typedef enum {
 void insertion_sort(void* arr, int len, data_type type);
 void swap(void* a, void* b, size_t size);
 int compare_value(void* a, void* b, data_type type);
+size_t data_size(data_type type);
+
 
 int main(void) {
 
 	int int_arr[] = { -5, 7, -2, 10, 5, -3, 9, 8 };
-	float float_arr[] = { 3.14, -1.5, 0.0, 2.71, -8.25, 5.5 };
-	char char_arr[] = { "Hello World" };
+	float float_arr[] = { 3.14, -1.5, 0, 2.71, -8.25, 5.5 };
+	char char_arr[] = "Hello World";
 
 	char data_type = 'c';
 
 	if (data_type == 'i') {
 		int len = sizeof(int_arr) / sizeof(int_arr[0]);
-		insertion_sort(int_arr, len, type_char);
+		insertion_sort(int_arr, len, type_int);
 		for (int i = 0; i < len; ++i) {
-			printf("%d", int_arr[i]);
+			printf("%d ", int_arr[i]);
 		}
 	}
 	else if (data_type == 'f') {
 		int len = sizeof(float_arr) / sizeof(float_arr[0]);
 		insertion_sort(float_arr, len, type_float);
 		for (int i = 0; i < len; ++i) {
-			printf("%f", float_arr[i]);
+			printf("%.2f ", float_arr[i]);
 		}
 	}
 	else if (data_type == 'c') {
 		int len = sizeof(char_arr) / sizeof(char_arr[0]);
 		insertion_sort(char_arr, len, type_char);
 		for (int i = 0; i < len; ++i) {
-			printf("%c", char_arr[i]);
+			printf("%c ", char_arr[i]);
 		}
 	}
 	return 0;
 }
 
 void insertion_sort(void* arr, int len, data_type type){
-	size_t size_in_bytes;
-	switch (type) {
-	case type_int:
-		size_in_bytes = sizeof(int);
-		break;
-	case type_float:
-		size_in_bytes = sizeof(float);
-		break;
-	case type_char:
-		size_in_bytes = sizeof(char);
-		break;
-	}
+	size_t size_in_bytes = data_size(type);
 	for (int i = 1; i < len; i++) {
 		for (int j = i; j > 0; --j) {
 			void* current = (char*)arr + j * size_in_bytes;
@@ -68,6 +59,17 @@ void insertion_sort(void* arr, int len, data_type type){
 				break;
 			}
 		}
+	}
+}
+
+size_t data_size(data_type type) {
+	switch (type) {
+	case type_int:
+		return sizeof(int);
+	case type_float:
+		return sizeof(float);
+	case type_char:
+		return sizeof(char);
 	}
 }
 
